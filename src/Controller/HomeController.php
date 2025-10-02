605f0a44cbd32685ce0d77c33ba32ea20c4a10cf
blinky.pink <ed_coste@hotmail.com>
2025-10-02 21:49:00 +0200

    page d'accueil mie parcour

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        // --- Page D'accueil ---
+        return $this->render('home/index.html.twig', [
+            'page_title' => 'Bienvenue sur Hells Hardrock !',
+        ]);
+    }
+}
+//--- /home défini la route page d'accueil ---
+//--- rend le template 'home/index.title.html.twig' ---
+//--- envoie une variable 'page_title' à twig ---
